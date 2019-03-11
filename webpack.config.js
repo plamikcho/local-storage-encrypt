94b561c3ea2eaeeb3a48f0769d3cd06b20a13060
Plamen Dzhorev <pdzhorev@axway.com>
2019-03-11 13:23:04 +0200

    Babel config for browser support

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,17 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: ['@babel/polyfill', './src/index.js'],
   },
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
     hot: true
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
