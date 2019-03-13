e3fef2bf88768bf6164999244a1edd9912845d69
Plamen Dzhorev <pdzhorev@axway.com>
2019-03-13 15:43:43 +0200

    Setting up library

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,10 @@ const common = require('./webpack.base.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    library: 'pbcrypto',
+    libraryTarget: 'umd', // universal module definition
+    // the type of the exported library
+    globalObject: 'this',
+  }
 });
