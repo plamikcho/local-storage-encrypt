6264c56a85d2b8d8eaeed2753c2df740dee9f20c
Plamen Dzhorev <pdzhorev@axway.com>
2019-03-25 10:53:43 +0200

    Browser support restored for getEncryptedStorage to ensure consistency across browsers. Used approach is feature detection using small piece of data.

--- src/encrypted-storage.js
+++ src/encrypted-storage.js
@@ -2,12 +2,31 @@ import { ab2str8, str2ab8 } from './encoder';
 import { getPbCrypto } from './crypto';
 
 /**
+ * Determines whether current browser supports WebCrypto API using feature detection
+ */
+const isBrowserSupported = async () => {
+  const testMessage = 'w?';
+  try {
+    const cryptoWrapper = getPbCrypto('a', 'b');
+    const iv = cryptoWrapper.getIv();
+    const encrypted = await cryptoWrapper.encrypt(testMessage, iv);
+    const decrypted = await cryptoWrapper.decrypt(encrypted, iv);
+    return decrypted === testMessage;
+  } catch (error) {
+    console.warn('Your browser does not support WebCrypto API', error);
+    return false;
+  }
+};
+
+/**
  * Gets encrypted storage with async getItem and setItem
  * 
  * @param {Storage} storage Browser storage - localStorage, sessionStorage
  * @param {ICrypto} cryptoWrapper Crypto
  */
 export const getEncryptedStorageFromCrypto = (storage, cryptoWrapper) => {
+
+  let isSupported;
 
   const getIvKey = key => `${key}_iv`;
 
@@ -23,34 +42,50 @@ export const getEncryptedStorageFromCrypto = (storage, cryptoWrapper) => {
     },
   };
 
+  const setBrowserSupport = async () => {
+    if (typeof isSupported === 'undefined') {
+      isSupported = await isBrowserSupported();
+    }
+  };
+
   return {
     async setItem(key, value) {
-      try {
-        const iv = cryptoWrapper.getIv(); // getting iv per item
-        const encrypted = await cryptoWrapper.encrypt(value, iv);
-        storage.setItem(key, String(encrypted));
-        storage.setItem(getIvKey(key), ab2str8(iv));
-      }
-      catch (error) {
-        console.error(`Cannot set encrypted value for ${key}. Error: ${error}`);
-        throw error;
+      await setBrowserSupport();
+      if (isSupported) {
+        try {
+          const iv = cryptoWrapper.getIv(); // getting iv per item
+          const encrypted = await cryptoWrapper.encrypt(value, iv);
+          storage.setItem(key, String(encrypted));
+          storage.setItem(getIvKey(key), ab2str8(iv));
+        }
+        catch (error) {
+          console.error(`Cannot set encrypted value for ${key}. Error: ${error}`);
+          throw error;
+        }
+      } else {
+        storage.setItem(key, value); // legacy mode, no encryption
       }
     },
     async getItem(key) {
-      try {
-        const data = storage.getItem(key);
-        const iv = storage.getItem(getIvKey(key));
-        const decrypted = await cryptoWrapper.decrypt(data, str2ab8(iv));
-        return decrypted;
+      await setBrowserSupport();
+      if (isSupported) {
+        try {
+          const data = storage.getItem(key);
+          const iv = storage.getItem(getIvKey(key));
+          const decrypted = await cryptoWrapper.decrypt(data, str2ab8(iv));
+          return decrypted;
+        }
+        catch (error) {
+          console.error(`Cannot get encrypted item for ${key}. Error: ${error}`);
+          return null;
+        }
       }
-      catch (error) {
-        console.error(`Cannot get encrypted item for ${key}. Error: ${error}`);
-        return null;
-      }
+      return storage.getItem(key); // legacy mode, no encryption
     },
     removeItem(key) {
       storage.removeItem(key);
-      storage.removeItem(getIvKey(key));
+      const ivKey = getIvKey(key);
+      storage.getItem(ivKey) && storage.removeItem(ivKey);
     },
     ...unmodifiedFunctions,
   };
